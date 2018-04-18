b07bcc7861769461226ccf558910644e62d8a4ee
Jillness <jillaseymour@mail.com>
2018-04-18 12:43:24 -0700

    Sentiment analysis included for Hot 100

---
+++ config.py
@@ -0,0 +1,10 @@
+#spotify keys 
+spotify_client_ID = 'ca82095e94484c588f5affd4401461e4'
+spotify_client_secret = '499f313063354f99aa15eae774b3b284'
+my_spotify_uri = 'spotify:user:beenjill'
+my_redirect_url = 'http://localhost:8888/'
+username= 'beenjill'
+scope='user-library-read'
+
+#openweathermapy key
+weather_api = "25bc90a1196e6f153eece0bc0b0fc9eb"
